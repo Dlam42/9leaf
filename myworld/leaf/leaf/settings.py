a30e06348ecf46d58022d9210ccada13820e9cae
MinNgo123 <166904795+MinNgo123@users.noreply.github.com>
2025-04-26 11:36:54 -0700

    Update settings.py

--- myworld/leaf/leaf/settings.py
+++ myworld/leaf/leaf/settings.py
@@ -7,11 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/5.1/ref/settings/f
+https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o4kqu$9a8q=#xsq-w^04tohm5*6q#u=)r^wg@&e9mgv526@s-9'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -132,5 +135,3 @@ SESSION_SAVE_EVERY_REQUEST = True  # Ensure session is saved on every request
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
